e5857b63bb1d02d7b0698c368f4e6810e0a9de63
Ricardo Dias <ricardodias.dev@gmail.com>
2023-01-19 09:45:02 -0300

    Estilos globais

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import GlobalStyles from './styles/GlobalStyles'
+import Dashboard from './pages/Dashboard'
+
+const App = () => {
+  return (
+    <>
+        <GlobalStyles/>
+        <Dashboard/>
+    </>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Dashboard from './pages/Dashboard';
+import App from './App';
 
 
 
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Dashboard />
+    <App />
   </React.StrictMode>
 );
 
