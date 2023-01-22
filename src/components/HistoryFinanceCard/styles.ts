55ee22199d67948fa3d883ba8eaf40a1fb05ef07
Ricardo Dias <ricardodias.dev@gmail.com>
2023-01-22 19:51:42 -0300

    Listando as entradas e saídas

--- src/components/HistoryFinanceCard/styles.ts
+++ src/components/HistoryFinanceCard/styles.ts
@@ -6,7 +6,7 @@ interface ITagProps {
 export const Container = styled.li`
     background-color: ${props => props.theme.colors.tertiary};
     list-style: none;
-    border-radius: 5px;
+    border-radius: 10px;
     margin: 10px 0;
     padding: 12px 10px;
     display: flex;
@@ -28,13 +28,19 @@ export const Container = styled.li`
         padding-left: 10px;
     }
 
+    >div span {
+        font-weight: bold;
+        font-size: 22px;
+
+    }
+
     
 
 `
 export const Tag = styled.div<ITagProps>`
     position: absolute;
     left: 0;
-    width: 10px;
+    width: 13px;
     height: 60%;
     background-color: ${props => props.color};
 `
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,69 +1,108 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState, useEffect } from "react";
 
-import { Container, Content, Filters } from './styles'
-import ContentHeader from '../../components/ContentHeader'
-import SelectInput from '../../components/SelectInput'
-import HistoryFinanceCard from '../../components/HistoryFinanceCard'
-import { useParams } from 'react-router-dom'
+import { Container, Content, Filters } from "./styles";
+import ContentHeader from "../../components/ContentHeader";
+import SelectInput from "../../components/SelectInput";
+import HistoryFinanceCard from "../../components/HistoryFinanceCard";
+import { useParams } from "react-router-dom";
+
+import gains from "../../repositories/gains";
+import expenses from "../../repositories/expenses";
+
+interface IData {
+  id: string,
+  description: string;
+  amountFormatted: string;
+  frequency: string;
+  dataFormatted: string;
+  tagColor: string;
+}
 
 const List: React.FC = () => {
-  const {type} = useParams()
+
+
+  const [data, setData] = useState<IData[]>([]);
+  const { type } = useParams();
 
   const title = useMemo(() => {
-    return type=== 'entry-balance' ? 'Entradas' : 'Saídas'
-  }, [type])
-    
+    return type === "entry-balance" ? "Entradas" : "Saídas";
+  }, [type]);
 
   const lineColor = useMemo(() => {
-    return type=== 'entry-balance' ? '#F7931B' : '#E44C4E'
-  }, [type])
+    return type === "entry-balance" ? "#F7931B" : "#E44C4E";
+  }, [type]);
+
+  const listData =  useMemo(() => {
+    return type === 'entry-balance' ? gains : expenses;
+  },[type])
 
   const months = [
-    {value: 1, label:'janeiro'}, 
-    {value: 2, label:'fevereiro'},
-    {value: 3, label:'março'}, 
-    {value: 4, label:'abril'},
-    {value: 5, label:'maio'}, 
-    {value: 6, label:'junho'},
-    {value: 7, label:'julho'}, 
-    {value: 8, label:'agosto'},
-    {value: 9, label:'setembro'}, 
-    {value: 10, label:'outubro'},
-    {value: 11, label:'novembro'}, 
-    {value: 12, label:'dezembro'},
-  ]
+    { value: 1, label: "janeiro" },
+    { value: 2, label: "fevereiro" },
+    { value: 3, label: "março" },
+    { value: 4, label: "abril" },
+    { value: 5, label: "maio" },
+    { value: 6, label: "junho" },
+    { value: 7, label: "julho" },
+    { value: 8, label: "agosto" },
+    { value: 9, label: "setembro" },
+    { value: 10, label: "outubro" },
+    { value: 11, label: "novembro" },
+    { value: 12, label: "dezembro" },
+  ];
 
   const years = [
-    {value:2023, label: 2023},
-    {value:2022, label: 2022},
-    {value:2021, label: 2021},
-    {value:2020, label: 2020},
-  ]
+    { value: 2023, label: 2023 },
+    { value: 2022, label: 2022 },
+    { value: 2021, label: 2021 },
+    { value: 2020, label: 2020 },
+  ];
+
+  useEffect(() => {
+    const response = listData.map((item) => {
+      return {
+        id: String(Math.random() * listData.length),
+        description: item.description,
+        amountFormatted: item.amount,
+        frequency: item.frequency,
+        dataFormatted: item.date,
+        tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E',
+      }
+    })
+    setData(response)
+    
+  },[type, listData])
 
   return (
     <Container>
-        <ContentHeader title={title} lineColor={lineColor}>
-          <SelectInput options={months}/>
-          <SelectInput options={years}/>
-        </ContentHeader>
-        <Filters>
-          <button type='button' className='tag-filter tag-filter-recurrent'>
-            Recorrentes
-          </button>
-          <button type='button' className='tag-filter tag-filter-eventual'>
-            Eventuais
-          </button>
-        </Filters>
-        <Content>
-          <HistoryFinanceCard
-            tagColor='#E44C4E'
-            title='Conta de luz'
-            subtitle='27/07/2022'
-            amount='R$ 130,00'
-          />
-        </Content>
-    </Container>
-  )
-}
+      <ContentHeader title={title} lineColor={lineColor}>
+        <SelectInput options={months} />
+        <SelectInput options={years} />
+      </ContentHeader>
+      <Filters>
+        <button type="button" className="tag-filter tag-filter-recurrent">
+          Recorrentes
+        </button>
+        <button type="button" className="tag-filter tag-filter-eventual">
+          Eventuais
+        </button>
+      </Filters>
+      <Content>
+        {
+          data.map(item => (
+            <HistoryFinanceCard
+              key={item.id}
+              tagColor={item.tagColor}
+              title={item.description}
+              subtitle={item.dataFormatted}
+              amount={item.amountFormatted}
+            />
 
-export default List
+          ))
+        }
+      </Content>
+    </Container>
+  );
+};
+
+export default List;
