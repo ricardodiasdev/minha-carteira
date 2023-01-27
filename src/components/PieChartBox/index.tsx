f9ea47f48f7d58219b7adaf3935ddeca5b3e1e1a
Ricardo Dias <ricardodias.dev@gmail.com>
2023-01-26 22:04:07 -0300

    Gráfico de Pizza

---
+++ src/components/PieChartBox/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import {PieChart, Pie, Cell, ResponsiveContainer} from 'recharts'
+import { 
+    Container, 
+    SideLeft, 
+    LegendContainer, 
+    Legend, 
+    SideRight
+} from './styled'
+
+interface IPieChartBoxProps {
+    data: {
+        name: string;
+        value: number;
+        percent: number;
+        color: string;
+    }[]
+}
+
+const PieChartBox: React.FC<IPieChartBoxProps> =  ({data}) => (
+    <Container>
+        <SideLeft>
+            <h2>Relação</h2>
+            <LegendContainer>
+                {
+                    data.map(item => (
+                        <Legend key={item.name} color={item.color}>
+                            <div>{item.percent}</div>
+                            <span>{item.name}</span>
+                        </Legend>
+                    ))
+                }
+                
+            </LegendContainer>
+        </SideLeft>
+        <SideRight>
+            <ResponsiveContainer >
+                <PieChart >
+                    <Pie dataKey="value" data={data}>
+                        {
+                            data.map(item => (
+                                <Cell key={item.name} fill={item.color} />
+                            ))
+                        }
+                    </Pie>
+                </PieChart>
+            </ResponsiveContainer>
+        </SideRight>
+
+    </Container>
+  )
+
+
+export default PieChartBox
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,26 +1,29 @@
-import React, { useMemo, useState } from 'react'
+import React, { useMemo, useState } from "react";
 
-import SelectInput from '../../components/SelectInput'
-import ContentHeader from '../../components/ContentHeader'
-import WalletBox from '../../components/WalletBox'
-import MessageBox from '../../components/Messagebox'
+import SelectInput from "../../components/SelectInput";
+import ContentHeader from "../../components/ContentHeader";
+import WalletBox from "../../components/WalletBox";
+import MessageBox from "../../components/Messagebox";
+import PieChartBox from "../../components/PieChartBox";
 
-import expenses from '../../repositories/expenses'
-import gains from '../../repositories/gains'
-import listOfMonths from '../../utils/months'
+import expenses from "../../repositories/expenses";
+import gains from "../../repositories/gains";
+import listOfMonths from "../../utils/months";
 
-import happy from '../../assets/happy.svg'
-import sad from '../../assets/sad.svg'
-import grinning from '../../assets/grinning.svg'
+import happy from "../../assets/happy.svg";
+import sad from "../../assets/sad.svg";
+import grinning from "../../assets/grinning.svg";
 
-
-import { Container, Content } from './styles'
+import { Container, Content } from "./styles";
 
 const Dashboard: React.FC = () => {
-  
-  const [monthSelected, setMonthSelected] = useState<number>(new Date().getMonth() + 1);
-  const [yearSelected, setYearSelected] = useState<number>(new Date().getFullYear());
-  
+  const [monthSelected, setMonthSelected] = useState<number>(
+    new Date().getMonth() + 1
+  );
+  const [yearSelected, setYearSelected] = useState<number>(
+    new Date().getFullYear()
+  );
+
   const years = useMemo(() => {
     let uniqueYears: number[] = [];
     [...expenses, ...gains].forEach((item) => {
@@ -52,121 +55,141 @@ const Dashboard: React.FC = () => {
   }, []);
 
   const totalExpenses = useMemo(() => {
-    
-    let  total: number = 0;
-    
-    expenses.forEach(item => {
+    let total: number = 0;
+
+    expenses.forEach((item) => {
       const date = new Date(item.date);
       const year = date.getFullYear();
       const month = date.getMonth() + 1;
 
-      if(month === monthSelected && year === yearSelected) {
+      if (month === monthSelected && year === yearSelected) {
         try {
-          total += Number(item.amount)
+          total += Number(item.amount);
         } catch (error) {
-          throw new Error('Invalid amount. Amount must be number.')
+          throw new Error("Invalid amount. Amount must be number.");
         }
       }
-    })
-    return total
-  },[monthSelected, yearSelected])
+    });
+    return total;
+  }, [monthSelected, yearSelected]);
 
   const totalGains = useMemo(() => {
-    
-    let  total: number = 0;
-    
-    gains.forEach(item => {
+    let total: number = 0;
+
+    gains.forEach((item) => {
       const date = new Date(item.date);
       const year = date.getFullYear();
       const month = date.getMonth() + 1;
 
-      if(month === monthSelected && year === yearSelected) {
+      if (month === monthSelected && year === yearSelected) {
         try {
-          total += Number(item.amount)
+          total += Number(item.amount);
         } catch (error) {
-          throw new Error('Invalid amount. Amount must be number.')
+          throw new Error("Invalid amount. Amount must be number.");
         }
       }
-    })
-    return total
-  },[monthSelected, yearSelected])
+    });
+    return total;
+  }, [monthSelected, yearSelected]);
 
   const totalBalance = useMemo(() => {
-    return totalGains - totalExpenses
-  }, [totalExpenses, totalGains ])
+    return totalGains - totalExpenses;
+  }, [totalExpenses, totalGains]);
 
   const message = useMemo(() => {
-    if(totalBalance < 0) {
+    if (totalBalance < 0) {
       return {
-      title: 'Que tristeza!',
-        description: 'Nesse mês, você gastou mais do que deveria.',
-        footerText: 'Verifique seus gastos e tente cortar algumas despesas desnecessárias.',
-        icon: sad
-      }
-    }
-    else if(totalBalance === 0) {
+        title: "Que tristeza!",
+        description: "Nesse mês, você gastou mais do que deveria.",
+        footerText:
+          "Verifique seus gastos e tente cortar algumas despesas desnecessárias.",
+        icon: sad,
+      };
+    } else if (totalBalance === 0) {
       return {
-        title: 'Ufaa!',
-        description: 'Nesse mês, você gastou exatamente o que ganhou.',
-        footerText: 'No próximo mês tente cortar algumas despesas desnecessárias.',
-        icon: grinning
-      }
-    }
-    else {
+        title: "Ufaa!",
+        description: "Nesse mês, você gastou exatamente o que ganhou.",
+        footerText:
+          "No próximo mês tente cortar algumas despesas desnecessárias.",
+        icon: grinning,
+      };
+    } else {
       return {
-        title: 'Muito bem!',
-        description: 'Sua carteira está positiva!',
-        footerText: 'Continue assim. Considere investir o seu saldo.',
-        icon: happy
-      }
+        title: "Muito bem!",
+        description: "Sua carteira está positiva!",
+        footerText: "Continue assim. Considere investir o seu saldo.",
+        icon: happy,
+      };
     }
-  },[totalBalance])
+  }, [totalBalance]);
+
+  const relationExpensesVersusGains = useMemo(() => {
+    const total: number = totalGains + totalExpenses;
+    const percentGains = (totalGains / total) * 100;
+    const percentExpenses = (totalExpenses / total) * 100;
+    const data = [
+      {
+        name: "Entradas",
+        value: totalGains,
+        percent: Number(percentGains.toFixed(1)),
+        color: "#F7931B",
+      },
+      {
+        name: "Saídas",
+        value: totalExpenses,
+        percent: Number(percentExpenses.toFixed(1)),
+        color: "#E44C4E",
+      }
+    ];
+    return data;
+  }, [totalGains, totalExpenses]);
 
   return (
     <Container>
-        <ContentHeader title='Dashboard' lineColor='#F7931B'>
-          <SelectInput
-            options={months}
-            onChange={(e) => setMonthSelected(Number(e.target.value))}
-            defaultValue={monthSelected}
-          />
-          <SelectInput
-            options={years}
-            onChange={(e) => setYearSelected(Number(e.target.value))}
-            defaultValue={yearSelected}
-          />
-        </ContentHeader>
-        <Content>
-          <WalletBox
-            color='#4E41F0'
-            title='saldo'
-            amount={totalBalance}
-            footerlabel = "atualizado com base nas entradas e saídas"
-            icon='dolar'
-          />
-           <WalletBox
-            color='#F7931B'
-            title='entradas'
-            amount={totalGains}
-            footerlabel = "atualizado com base nas entradas e saídas"
-            icon='arrowUp'
-          />
-           <WalletBox
-            color='#E44C4E'
-            title='saídas'
-            amount={totalExpenses}
-            footerlabel = "atualizado com base nas entradas e saídas"
-            icon='arrowDown'
-          />
-          <MessageBox 
-            title= {message.title}
-            description= {message.description}
-            footerText= {message.footerText}
-            icon= {message.icon}
-          />
-        </Content>
+      <ContentHeader title="Dashboard" lineColor="#F7931B">
+        <SelectInput
+          options={months}
+          onChange={(e) => setMonthSelected(Number(e.target.value))}
+          defaultValue={monthSelected}
+        />
+        <SelectInput
+          options={years}
+          onChange={(e) => setYearSelected(Number(e.target.value))}
+          defaultValue={yearSelected}
+        />
+      </ContentHeader>
+      <Content>
+        <WalletBox
+          color="#4E41F0"
+          title="saldo"
+          amount={totalBalance}
+          footerlabel="atualizado com base nas entradas e saídas"
+          icon="dolar"
+        />
+        <WalletBox
+          color="#F7931B"
+          title="entradas"
+          amount={totalGains}
+          footerlabel="atualizado com base nas entradas e saídas"
+          icon="arrowUp"
+        />
+        <WalletBox
+          color="#E44C4E"
+          title="saídas"
+          amount={totalExpenses}
+          footerlabel="atualizado com base nas entradas e saídas"
+          icon="arrowDown"
+        />
+        <MessageBox
+          title={message.title}
+          description={message.description}
+          footerText={message.footerText}
+          icon={message.icon}
+        />
+        <PieChartBox data={relationExpensesVersusGains} />
+      </Content>
     </Container>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
