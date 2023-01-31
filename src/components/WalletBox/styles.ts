53af01efcee934fdd451a6f9395b1d11274d736b
Ricardo Dias <ricardodias.dev@gmail.com>
2023-01-31 11:46:50 -0300

    Animações

--- src/components/WalletBox/styles.ts
+++ src/components/WalletBox/styles.ts
@@ -1,8 +1,23 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 interface IContainerProps {
   color: string;
 }
+
+const animate = keyframes`
+  0% {
+    transform: translateX(-100px);
+    opacity: 0;
+  }
+  50% {
+    opacity: .3;
+  }
+
+  100%{
+    transform: translateX(0px);
+    opacity: 1;
+  }
+`
 
 export const Container = styled.div<IContainerProps>`
   width: 32%;
@@ -17,6 +32,8 @@ export const Container = styled.div<IContainerProps>`
 
   position: relative;
   overflow: hidden;
+
+  animation: ${animate} .5s;
 
   > img {
     height: 110%;
